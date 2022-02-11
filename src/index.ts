ad6f208b7d91134f6d554b7c7492c912968d9e0a
Joan Grau <jgrau@peersyst.com>
2022-02-11 12:35:53 +0100

    add wallet baalance and address

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-import { RPC } from "@ckb-lumos/lumos";
+import { Indexer, RPC } from "@ckb-lumos/lumos";
 import { Wallet } from "./wallet";
 
-const uri = "http://78.46.174.87:8114/rpc";
-// const indexer = "http://78.46.174.87:8114/indexer";
-const rpc = new RPC(uri);
-rpc.get_block_by_number("0x100").then((block) => console.log("block:\n", block));
-rpc.get_current_epoch().then((epoch) => console.log("epoch:\n", epoch));
+const ckbUrl = "http://78.46.174.87:8114/rpc";
+const indexerUrl = "http://78.46.174.87:8114/indexer";
+// const ckbUrl = "https://testnet.ckb.dev/rpc";
+// const indexerUrl = "https://testnet.ckb.dev/indexer";
+
+const rpc = new RPC(ckbUrl);
+const indexer = new Indexer(indexerUrl, ckbUrl);
+
+// rpc.get_current_epoch().then((epoch) => console.log("epoch:\n", epoch));
 rpc.get_blockchain_info().then((info) => console.log("info:\n", info));
+rpc.get_block_by_number("0x42a724").then((block) => console.log("block:\n", block));
 
 // Create wallet
-const wallet = Wallet.create();
-Wallet.importFromMnemonic(wallet.mnemo);
+const wallet = new Wallet("teach act exotic into script once dutch choice menu elite apple faith");
+const address = wallet.getAddress();
+console.log(address);
+wallet.getBalance(indexer, address).then((value) => console.log(value));
--- src/wallet.ts
+++ src/wallet.ts
@@ -1,35 +1,49 @@
-import { mnemonic, ExtendedPrivateKey, ExtendedPublicKey, AccountExtendedPublicKey, AddressType } from "@ckb-lumos/hd";
+import { mnemonic, ExtendedPrivateKey, AccountExtendedPublicKey, AddressType } from "@ckb-lumos/hd";
+import { config, helpers } from "@ckb-lumos/lumos";
+import { Indexer } from "@ckb-lumos/base";
+
+// AGGRON4 for test, LINA for main
+const { AGGRON4 } = config.predefined;
 
 export class Wallet {
-    public mnemo: string;
-    public privateKey: ExtendedPrivateKey;
-    public publicKey: ExtendedPublicKey;
+    private privateKey: ExtendedPrivateKey;
     public accountPublicKey: AccountExtendedPublicKey;
 
-    static create(): Wallet {
-        const wallet = new Wallet();
-        wallet.mnemo = mnemonic.generateMnemonic();
-        const seed = mnemonic.mnemonicToSeedSync(wallet.mnemo);
-        wallet.privateKey = ExtendedPrivateKey.fromSeed(seed);
-        wallet.publicKey = wallet.privateKey.toExtendedPublicKey();
-        wallet.accountPublicKey = wallet.privateKey.toAccountExtendedPublicKey();
-        console.log(AccountExtendedPublicKey.ckbAccountPath);
-        console.log(wallet.accountPublicKey.publicKeyInfo(AddressType.Change, 0));
-        console.log(wallet.accountPublicKey.publicKeyInfo(AddressType.Receiving, 0));
-        console.log(wallet);
-
-        return wallet;
+    constructor(private readonly mnemo?: string) {
+        if (!this.mnemo) {
+            this.mnemo = mnemonic.generateMnemonic();
+        }
+        const seed = mnemonic.mnemonicToSeedSync(this.mnemo);
+        this.privateKey = ExtendedPrivateKey.fromSeed(seed);
+        this.accountPublicKey = this.privateKey.toAccountExtendedPublicKey();
     }
 
-    static importFromMnemonic(mnemo: string): Wallet {
-        const wallet = new Wallet();
-        wallet.mnemo = mnemo;
-        const seed = mnemonic.mnemonicToSeedSync(wallet.mnemo);
-        wallet.privateKey = ExtendedPrivateKey.fromSeed(seed);
-        wallet.publicKey = wallet.privateKey.toExtendedPublicKey();
-        wallet.accountPublicKey = wallet.privateKey.toAccountExtendedPublicKey();
-        console.log(wallet);
+    getAddress(accountId = 0): string {
+        const template = AGGRON4.SCRIPTS["SECP256K1_BLAKE160"];
+        const lockScript = {
+            code_hash: template.CODE_HASH,
+            hash_type: template.HASH_TYPE,
+            args: this.accountPublicKey.publicKeyInfo(AddressType.Receiving, accountId).blake160,
+        };
 
-        return wallet;
+        return helpers.generateAddress(lockScript, { config: AGGRON4 });
+    }
+
+    async getBalanceFromAccount(indexer: Indexer, accountId = 0): Promise<number> {
+        const address = this.getAddress(accountId);
+        return this.getBalance(indexer, address);
+    }
+
+    async getBalance(indexer: Indexer, address: string): Promise<number> {
+        const collector = indexer.collector({
+            lock: helpers.parseAddress(address, { config: AGGRON4 }),
+        });
+
+        let balance = 0;
+        for await (const cell of collector.collect()) {
+            balance += parseInt(cell.cell_output.capacity, 16);
+        }
+
+        return balance;
     }
 }
